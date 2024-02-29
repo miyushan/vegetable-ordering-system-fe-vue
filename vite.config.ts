f8ffd9c17c7abf6024fddcb1d97fce6c35bcbc7b
Miyushan <smiyushan2015@gmail.com>
2024-02-29 14:05:29 +0530

    BalmUI config added

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,9 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      'balm-ui-plus': 'balm-ui/dist/balm-ui-plus.esm.js',
+      'balm-ui-css': 'balm-ui/dist/balm-ui.css'
     }
   }
 })
