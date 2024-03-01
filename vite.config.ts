28288c2fad1c258c9df850409eab2fdfaf11aa6e
Miyushan <smiyushan2015@gmail.com>
2024-03-01 16:20:32 +0530

    shadcn-vue library added

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,20 @@
-import { fileURLToPath, URL } from 'node:url'
-
+import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()]
+    }
+  },
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': path.resolve(__dirname, './src')
     }
   }
 })
