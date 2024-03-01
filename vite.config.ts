ff71306d5e19be91ef9cb95f484a1ea75ae8518e
Miyushan <smiyushan2015@gmail.com>
2024-03-01 16:33:45 +0530

    Revert "shadcn-vue library added"
    This reverts commit 28288c2fad1c258c9df850409eab2fdfaf11aa6e.

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,14 @@
-import path from 'path'
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-import tailwind from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [tailwind(), autoprefixer()]
-    }
-  },
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
